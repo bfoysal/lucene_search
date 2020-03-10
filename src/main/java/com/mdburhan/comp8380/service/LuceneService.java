1a930804fbf7aa86e75e174809d4af4c5d8673df
burhan <burhan420@gmail.com>
2020-03-10 17:58:01 -0400

    pagerank added to index

--- src/main/java/com/mdburhan/comp8380/service/LuceneService.java
+++ src/main/java/com/mdburhan/comp8380/service/LuceneService.java
@@ -22,11 +22,15 @@ import org.apache.lucene.store.FSDirectory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.print.Doc;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author burhan <burhan420@gmail.com>
@@ -39,13 +43,19 @@ public class LuceneService {
     private String documentPath;
     @Value("${index.path}")
     private String indexPath;
+    @Value(("${rank.path}"))
+    private String pageRankPath;
 
     private static final String TITLE = "title";
     private static final String ABSTRACT = "abstract";
     private static final String CONTENTS = "contents";
+    private static final String ID = "id";
+    private static final String PAGE_RANK = "pr";
     static int counter = 0;
+    static Map<String, Double> pageRanks=new HashMap<String, Double>();
 
     public String createIndex() throws IOException {
+        pageRanks = getPageRank(pageRankPath+"/page_ranks.txt");
         Directory directory = FSDirectory.open(Paths.get(indexPath));
         Analyzer analyzer = new StandardAnalyzer();
         IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
@@ -54,6 +64,21 @@ public class LuceneService {
         indexWriter.close();
         return "Index Stored at "+indexPath;
     }
+
+    private Map<String, Double> getPageRank(String s) throws IOException {
+        Map<String,Double> rankMap = new HashMap<String, Double>();
+        InputStream stream = Files.newInputStream(Paths.get(s));
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+        String line;
+        while((line=br.readLine())!=null){
+            String []  pair = line.split(" ");
+            String key = pair[0];
+            double rank = Double.parseDouble(pair[1]);
+            rankMap.put(key,rank);
+        }
+        return rankMap;
+    }
+
     static void indexDocs(final IndexWriter writer, Path path) throws IOException {
 
         Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
@@ -78,7 +103,7 @@ public class LuceneService {
         //trying json streaming
         JsonFactory jsonFactory = new JsonFactory();
         JsonParser jsonParser = jsonFactory.createParser(file.toFile());
-        String titleString="", abstractString="" ,fieldName="";
+        String titleString="", abstractString="" ,fieldName="", idString="";
         JsonToken token;
         while (!jsonParser.isClosed()){
             token = jsonParser.nextToken();
@@ -92,10 +117,15 @@ public class LuceneService {
                     jsonParser.nextToken();
                     titleString=jsonParser.getValueAsString();
                 }
+                if (fieldName.equals(ID)){
+                    jsonParser.nextToken();
+                    idString=jsonParser.getValueAsString();
+                }
             }
             if(JsonToken.END_OBJECT.equals(token)){
                 doc.add(new StringField(TITLE,titleString,Field.Store.YES));
                 doc.add(new StringField(ABSTRACT,abstractString,Field.Store.YES));
+                doc.add(new DoubleDocValuesField(PAGE_RANK,pageRanks.get(idString)));
                 FieldType fieldType =new FieldType();
                 fieldType.setStoreTermVectors(true);
                 fieldType.setStoreTermVectorOffsets(true);
@@ -104,7 +134,7 @@ public class LuceneService {
                 doc.add(new Field(CONTENTS,titleString+abstractString,fieldType));
                 writer.addDocument(doc);
                 doc=new Document();
-                titleString=abstractString="";
+                titleString=abstractString=idString="";
                 counter++;
                 if (counter % 1000 == 0){
                     System.out.println("indexing "+counter+"-th document");
